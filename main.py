45976cf6ab1b87cdbbc924d2dbf465e8f8538947
andrewozhegov <andrew.ozhegov@yandex.ru>
2020-07-02 08:50:50 +0300

    update additional info in statusbar

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ from AddClientDialog import AddClientDialog
 
 class App(QMainWindow):
 
+    def update(self):
+        sum_client=0
+        for client in Clients.select():
+            sum_client += client.price
+        sum_cars=0
+        for car in Cars.select():
+            sum_cars += car.price
+        msg = 'Покупательская способность: ' + str(round(sum_client/sum_cars, 4))
+        self.statusBar().showMessage(msg)
+
     def __init__(self):
         super().__init__()
         self.title = 'CarStore'
@@ -29,12 +39,14 @@ class App(QMainWindow):
         button.setToolTip('don\'t push')
         button.move(100, 70)
         button.clicked.connect(self.on_click)
+        self.update()
         self.show()
 
     @pyqtSlot()
     def on_click(self):
         dialog = AddClientDialog()
         dialog.exec_()
+        self.update()
 
 
 if __name__ == '__main__':
