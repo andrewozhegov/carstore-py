65ce48367cdc918a67176e9a489bc81e854cc5e1
andrewozhegov <andrew.ozhegov@yandex.ru>
2020-07-02 08:53:10 +0300

    main menu init

--- main.py
+++ main.py
@@ -31,24 +31,75 @@ class App(QMainWindow):
         self.height = 480
         self.initUI()
 
+    def initMenu(self):
+        mainMenu = self.menuBar()
+
+        carsMenu = mainMenu.addMenu('Авто')
+
+        addCarButton = QAction(QIcon(), 'Добавить', self)
+        #addCarButton.setShortcut('Ctrl+N')
+        addCarButton.setStatusTip('Add client')
+        addCarButton.triggered.connect(self.on_addCar)
+        carsMenu.addAction(addCarButton)
+
+        remCarButton = QAction(QIcon(), 'Удалить', self)
+        #remCarButton.setShortcut('Ctrl+N')
+        remCarButton.setStatusTip('Add client')
+        remCarButton.triggered.connect(self.on_remCar)
+        carsMenu.addAction(remCarButton)
+
+
+        clientsMenu = mainMenu.addMenu('Клиенты')
+
+        addClientButton = QAction(QIcon(), 'Добавить', self)
+        #addClientButton.setShortcut('Ctrl+N')
+        addClientButton.setStatusTip('Add client')
+        addClientButton.triggered.connect(self.on_addClient)
+        clientsMenu.addAction(addClientButton)
+
+        remClientButton = QAction(QIcon(), 'Удалить', self)
+        #remClientButton.setShortcut('Ctrl+N')
+        remClientButton.setStatusTip('Add client')
+        remClientButton.triggered.connect(self.on_remClient)
+        clientsMenu.addAction(remClientButton)
+
+
+        additMenu = mainMenu.addMenu('Дополнительно')
+
+        updButton = QAction(QIcon(), 'Обновить', self)
+        #updButton.setShortcut('Ctrl+N')
+        updButton.setStatusTip('Add client')
+        updButton.triggered.connect(self.update)
+        additMenu.addAction(updButton)
+
+        exitButton = QAction(QIcon(), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        additMenu.addAction(exitButton)
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.initMenu()
 
-        button = QPushButton('button', self)
-        button.setToolTip('don\'t push')
-        button.move(100, 70)
-        button.clicked.connect(self.on_click)
         self.update()
         self.show()
 
     @pyqtSlot()
-    def on_click(self):
+    def on_addClient(self):
         dialog = AddClientDialog()
         dialog.exec_()
         self.update()
 
+    def on_remClient(self):
+        print("test1")
 
+    def on_addCar(self):
+        print("test2")
+
+    def on_remCar(self):
+        print("test3")
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
